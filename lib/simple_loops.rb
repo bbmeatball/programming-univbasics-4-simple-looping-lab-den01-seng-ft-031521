38ea2254053734fbcf0f48c8393aa0d7fb15ba1b
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-28 01:28:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,30 @@
 # Write your methods here
+def loop_message_five_times(string)
+  puts "Hello World."
+   puts "Hello World."
+    puts "Hello World."
+     puts "Hello World."
+      puts "Hello World."
+    end
+    
+def loop_message_n_times (string, interger)
+ counter=0
+ while counter <5 do
+   puts "Hello Moon."
+   counter+= 1
+  end
+  
+ counter=0 
+ while counter <10 do 
+   puts "Hello Red Balloon."
+   counter+= 1
+ end
+end
+
+def output_array(array)
+ array = ["Be yourself;", "everyone else is already taken", "- Oscar Wilde"]
+
+  puts array [1,2,3]
+  
+
+end
