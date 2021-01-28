c056b5d1ffce7731c3c4e03be24d4ce029e215c7
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-28 02:12:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -32,11 +32,7 @@ def output_array(array)
 end
   
 def return_string_array(array)
-  count=0
-  array=[5,4,3,2,1]
-  while count < array.length do
-  array[count].to_s
- count+=1 
- end
+  
+  
  array
 end
